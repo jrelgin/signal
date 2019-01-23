035658758d07073e2b1979a67d7656cf3bba2efe
Jason  Elgin <jason@jasons-mbp.attlocal.net>
2019-01-22 22:18:22 -0500

    adding scss

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Image from '../components/image'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
@@ -9,6 +10,9 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )

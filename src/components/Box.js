e1e3730c8a867c4d0c4be39f51d1b666019d4317
Jason Elgin <jason@signllantern.com>
2020-05-16 22:40:49 -0400

    fix another import

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './box.module.css';
+import styles from './Box.module.css';
 
 const box = (props) => {
   return <div className={props.skinny ? styles.skinnyBox : styles.box}>{props.children}</div>;

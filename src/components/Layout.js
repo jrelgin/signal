7b071843149801390cf506ee34eb5b86a350250e
Jason Elgin <jason@signllantern.com>
2020-05-16 22:37:26 -0400

    fix an import

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Layout.module.css';
-import Box from '../components/Box';
+import Box from './Box';
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`;

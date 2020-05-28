4135e7bf481c9ac506d7b2f34d23bb9ea7d92309
Jason Elgin <jason@signllantern.com>
2020-05-28 11:50:40 -0400

    update sketch and logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,28 +5,18 @@ import Box from './Box';
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`;
-  let header;
 
-  if (location.pathname === rootPath) {
-    header = (
-      <header className={styles.headerHome}>
-        <h1>
-          <Link to={`/`}>signal lantern</Link>
-        </h1>
-      </header>
-    );
-  } else {
-    header = (
-      <header className={styles.header}>
-        <h1>
-          <Link to={`/`}>signal lantern</Link>
-        </h1>
-      </header>
-    );
-  }
   return (
     <div className={styles.pageWrapper}>
-      <Box>{header}</Box>
+      <Box>
+        <header className={location.pathname === rootPath ? styles.headerHome : styles.header}>
+          <h1>
+            <Link to={`/`}>
+              signal lantern <span className={styles.period}>.</span>
+            </Link>
+          </h1>
+        </header>
+      </Box>
       <main>{children}</main>
 
       <nav className={styles.siteNav}>
